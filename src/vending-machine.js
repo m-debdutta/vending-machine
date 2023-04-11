4b16e0f5af613d178b5d8da2cfd818ff5a8e76a9
Debdutta <debdutta@Debduttas-MacBook-Pro.local>
2023-04-11 16:56:15 +0530

    Added feature to dispensing one and two rupee coins.

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,8 +1,8 @@
 // MAIN PROGRAM
 
 
-const getNumberOfCoins = function(amount) {
-  return amount;
+const dispenseOptimumCoins = function(amount) {
+  let coin = 0;
+  return (amount % 2 === 0) ? Math.trunc(amount / 2) : Math.trunc(amount / 2) + 1;
 }
-
-exports.getNumberOfCoins = getNumberOfCoins;
+exports.dispenseOptimumCoins = dispenseOptimumCoins;
--- test/test-vending-machine.js
+++ test/test-vending-machine.js
@@ -2,7 +2,10 @@
 
 const test = require('../lib/testing.js');
 const vend = require('../src/vending-machine.js');
-const getCoin = vend.getNumberOfCoins 
-test.assertArrayEqual(0, getCoin(0), "Zero rupees should give NO coin");
-test.assertArrayEqual(1, getCoin(1), "One rupees should give one coin");
-test.assertArrayEqual(5, getCoin(5), "Five rupees should give five coins");
+test.assertEqual(0, vend.dispenseOptimumCoins(0), "Zero rupees should give NO coin");
+test.assertEqual(1, vend.dispenseOptimumCoins(1), "One rupees should give one coin");
+test.assertEqual(1, vend.dispenseOptimumCoins(2), "Two rupees should give one coin");
+test.assertEqual(3, vend.dispenseOptimumCoins(5), "Five rupees should give three coins");
+test.assertEqual(3, vend.dispenseOptimumCoins(6), "Six rupees should give three coins");
+test.assertEqual(4, vend.dispenseOptimumCoins(7), "Seven rupees should give four coins");
+
