640c948fc24f795848d0b26293169d5e8fbe924a
Debdutta <m-debdutta@users.noreply.github.com>
2023-04-12 20:15:56 +0530

    Installing features to accept denominations in any oreder.

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,7 +1,7 @@
 // MAIN PROGRAM
 
 const dispenseOptimumCoins = function(amount, denominations) {
-  const orderedDenominations = denominations.reverse();
+  const orderedDenominations = sortList(denominations);
   let newAmount = amount;
   let coins = 0;
   for (const element of orderedDenominations) {
@@ -13,3 +13,19 @@ const dispenseOptimumCoins = function(amount, denominations) {
   return coins;
 }
 exports.dispenseOptimumCoins = dispenseOptimumCoins;
+
+const sortList = function(numbers) {
+  const list = numbers;
+  for (let i = 1; i < list.length; i++) {
+    for (let index = 0; index < list.length - i; index++) {
+      if(list[index] < list[index + 1]) {
+        const swapHand = list[index];
+        list[index] = list[index + 1];
+        list[index + 1] = swapHand;
+      }
+    } 
+  }
+  return list;
+}
+
+exports.sortList = sortList;
--- test/test-vending-machine.js
+++ test/test-vending-machine.js
@@ -3,22 +3,44 @@
 const test = require('../lib/testing.js');
 const vend = require('../src/vending-machine.js');
 
-test.assertEqual(0, vend.dispenseOptimumCoins(0,[1]), "Given zero rupees and 1 denomination, we should get NO coin");
-test.assertEqual(0, vend.dispenseOptimumCoins(5,[]), "Given five rupees and NO denomination, we should get NO coin");
+test.assertEqual(0, vend.dispenseOptimumCoins(0,[1]),
+"Given zero rupees and 1 denomination, we should get NO coin");
+test.assertEqual(0, vend.dispenseOptimumCoins(5,[]),
+"Given five rupees and NO denomination, we should get NO coin");
 
-test.assertEqual(1, vend.dispenseOptimumCoins(1,[1]), "Given one rupees and 1 denomination, we should get one coin");
-test.assertEqual(2, vend.dispenseOptimumCoins(2,[1]), "Given two rupees and 1 denomination, we should get two coins");
+test.assertEqual(1, vend.dispenseOptimumCoins(1,[1]), 
+"Given one rupees and 1 denomination, we should get one coin");
+test.assertEqual(2, vend.dispenseOptimumCoins(2,[1]), 
+"Given two rupees and 1 denomination, we should get two coins");
 
-test.assertEqual(3, vend.dispenseOptimumCoins(5,[1, 2]), "Given five rupees and 1,2 denomination, we should get three coins");
-test.assertEqual(2, vend.dispenseOptimumCoins(6,[1, 2, 5, 10]), "Given six rupees should give two coins");
-test.assertEqual(2, vend.dispenseOptimumCoins(7,[1, 2, 5, 10]), "Given seven rupees should give two coins");
+test.assertEqual(3, vend.dispenseOptimumCoins(5,[1, 2]), 
+"Given five rupees and 1,2 denomination, we should get three coins");
+test.assertEqual(2, vend.dispenseOptimumCoins(6,[1, 2, 5, 10]), 
+"Given six rupees should give two coins");
+test.assertEqual(2, vend.dispenseOptimumCoins(7,[1, 2, 5, 10]), 
+"Given seven rupees should give two coins");
 
-test.assertEqual(1, vend.dispenseOptimumCoins(10,[1, 2, 5, 10]), "Ten rupees should give one coins");
-test.assertEqual(2, vend.dispenseOptimumCoins(12,[1, 2, 5, 10]), "Twelve rupees should give two coins");
-test.assertEqual(2, vend.dispenseOptimumCoins(15,[1, 2, 5, 10]), "Fifteen rupees should give two coins");
-test.assertEqual(2, vend.dispenseOptimumCoins(15,[1, 2, 5, 10]), "Fifteen rupees should give two coins");
-test.assertEqual(4, vend.dispenseOptimumCoins(17,[1, 4, 6]), "Seventeen rupees should give four coins");
-test.assertEqual(2, vend.dispenseOptimumCoins(19,[1, 2, 13, 18]), "Nineteen rupees should give two coins");
+test.assertEqual(1, vend.dispenseOptimumCoins(10,[1, 2, 5, 10]), 
+"Ten rupees should give one coins");
+test.assertEqual(2, vend.dispenseOptimumCoins(12,[1, 2, 5, 10]), 
+"Twelve rupees should give two coins");
+test.assertEqual(2, vend.dispenseOptimumCoins(15,[1, 2, 5, 10]), 
+"Fifteen rupees should give two coins");
+test.assertEqual(2, vend.dispenseOptimumCoins(15,[1, 2, 5, 10]), 
+"Fifteen rupees should give two coins");
+test.assertEqual(4, vend.dispenseOptimumCoins(17,[1, 4, 6]), 
+"Seventeen rupees should give four coins");
+test.assertEqual(2, vend.dispenseOptimumCoins(19,[1, 2, 13, 18]), 
+"Nineteen rupees should give two coins");
 
-test.assertEqual(4, vend.dispenseOptimumCoins(13,[1, 4, 7]), "Thirteen rupees should give four coins");
+test.assertEqual(4, vend.dispenseOptimumCoins(13,[1, 4, 7]), 
+"Thirteen rupees should give four coins");
+
+test.assertEqual(4, vend.dispenseOptimumCoins(13,[7, 4, 1]), 
+"Thirteen rupees should give four coins");
+
+test.assertArrayEqual([ 7, 5, 3, 1 ],vend.sortList([5, 3, 7, 1]),
+"5, 3, 7, 1 should give 1, 3, 5, 7");
+test.assertEqual(2, vend.dispenseOptimumCoins(19,[2, 13, 18, 1]), 
+"Nineteen rupees should give two coins");
 
